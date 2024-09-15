371b9be1b436286788ccd9ecd151422bafd66a1c
Ayushi-Rahane <ayushirahane2021@gmail.com>
2024-09-15 19:25:03 +0530

    c

---
+++ _125Kth_Largest_Element_in_an_Array.java
@@ -0,0 +1,27 @@
+public class _125Kth_Largest_Element_in_an_Array {
+ /*  Time Limit Extended
+    public static int findKthLargest(int[] nums, int k) {
+        int temp;
+        for(int i=0;i<nums.length-1;i++){
+            for(int j=0;j<nums.length-1-i;j++){
+                  if(nums[j]>nums[j+1]){
+                        temp = nums[j];
+                        nums[j] = nums[j+1];
+                        nums[j+1] = temp;
+                  }
+            }
+        }
+         return nums[nums.length-k];
+    } */
+    public static int findKthLargest(int[] nums, int k){
+      
+    }
+
+    public static int 
+
+    public static void main(String[] args){
+        _125Kth_Largest_Element_in_an_Array f = new _125Kth_Largest_Element_in_an_Array();
+        int[] a ={3,2,3,1,2,4,5,5,6};
+        System.out.println(f.findKthLargest(a,4));
+    }
+}
